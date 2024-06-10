7cf31d9e6da40b194c2956d47838357f4f080aec
WJQSERVER <114663932+WJQSERVER@users.noreply.github.com>
2024-06-10 19:57:49 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,6 +4,6 @@ if [ ! -f /data/caddy/config/Caddyfile ]; then
     cp /data/caddy/Caddyfile /data/caddy/config/Caddyfile
 fi
 
-/data/caddy/caddy run --config /data/caddy/config/Caddyfile
+/data/caddy/caddy run --config /data/caddy/config/Caddyfile > /data/ipinfo/log/caddy.log 2>&1 &
 
-exec /data/ipinfo/ip
+/data/ipinfo/ip > /data/ipinfo/log/ip.log 2>&1 &

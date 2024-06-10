ac06f83356cfc7f8c77398085486f074793b07e1
里見 灯花 <wjqserverstudio@gmail.com>
2024-06-10 20:01:52 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -7,3 +7,7 @@ fi
 /data/caddy/caddy run --config /data/caddy/config/Caddyfile > /data/ipinfo/log/caddy.log 2>&1 &
 
 /data/ipinfo/ip > /data/ipinfo/log/ip.log 2>&1 &
+
+while [[ true ]]; do
+    sleep 1
+done    

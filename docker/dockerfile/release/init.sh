5d0052273c3c4348e0191ba5caa06649cd2c933d
WJQSERVER <wjqserver@outlook.com>
2024-12-04 20:59:43 +0800

    24w18b

--- docker/dockerfile/release/init.sh
+++ docker/dockerfile/release/init.sh
@@ -16,4 +16,10 @@ fi
 /data/caddy/caddy run --config /data/caddy/config/Caddyfile > /data/${APPLICATION}/log/caddy.log 2>&1 &
 
 # 启动 Go 应用
-/data/${APPLICATION}/${APPLICATION} -cfg /data/${APPLICATION}/config/config.toml
+/data/${APPLICATION}/${APPLICATION} -cfg /data/${APPLICATION}/config/config.toml > /data/${APPLICATION}/log/run.log 2>&1 &
+
+# 保持脚本运行
+while true; do
+    sleep 1
+done
+
